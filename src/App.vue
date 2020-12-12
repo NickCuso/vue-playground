901fb8a91ac78c0d339de7afe3546d2e8b5ed377
NickCuso <hardlydiff@gmail.com>
2020-12-12 11:31:48 -0800

    Sample countdown

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <BlockCountdown/>
+</template>
+
+<script>
+import BlockCountdown from './components/BlockCountdown.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BlockCountdown
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
